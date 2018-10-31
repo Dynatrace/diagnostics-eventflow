ec80a68e9a9b67e8d6d909751a885b2d12ad47b0
Patrick Thurner <patrick.thurner@gmail.com>
2018-10-31 15:40:08 +0100

    Extend DynatraceMetaData covering EventPushMessage properties

--- src/Microsoft.Diagnostics.EventFlow.Outputs.Dynatrace/Metadata/DynatraceEventData.cs
+++ src/Microsoft.Diagnostics.EventFlow.Outputs.Dynatrace/Metadata/DynatraceEventData.cs
@@ -16,7 +16,15 @@ namespace Microsoft.Diagnostics.EventFlow.Metadata
         public string Source { get; private set; }
         public string EventType { get; private set; }
         public string AnnotationType { get; private set; }
+        public string AnnotationTypeProperty { get; private set; }
         public string AnnotationDescription { get; private set; }
+        public string AnnotationDescriptionProperty { get; private set; }
+        public string Description { get; private set; }
+        public string DescriptionProperty { get; private set; }
+        public string DeploymentName { get; private set; }
+        public string DeploymentNameProperty { get; private set; }
+        public string DeploymentVersion { get; private set; }
+        public string DeploymentVersionProperty { get; private set; }
 
         // Ensure that DynatraceEventData can only be created using TryGetData() method
         private DynatraceEventData() { }
@@ -37,10 +45,40 @@ namespace Microsoft.Diagnostics.EventFlow.Metadata
             }
 
             meta = new DynatraceEventData();
-            meta.AnnotationType = eventMetadata["annotationType"];
-            meta.AnnotationDescription = eventMetadata["annotationDescription"];
             meta.Source = eventMetadata["source"];
             meta.EventType = eventMetadata["eventType"];
+            meta.AnnotationType = eventMetadata["annotationType"];
+            meta.AnnotationDescription = eventMetadata["annotationDescription"];
+            meta.Description = eventMetadata["description"];
+            meta.DeploymentName = eventMetadata["deploymentName"];
+            meta.DeploymentVersion = eventMetadata["deploymentVersion"];
+
+            string val = "";
+            if (!string.IsNullOrEmpty(eventMetadata["annotationTypeProperty"]))
+            {
+                if (eventData.GetValueFromPayload<string>("annotationTypeProperty", (v) => val = v))
+                    meta.AnnotationType = val;
+            }
+            if (!string.IsNullOrEmpty(eventMetadata["annotationDescriptionProperty"]))
+            {
+                if (eventData.GetValueFromPayload<string>("annotationDescriptionProperty", (v) => val = v))
+                    meta.AnnotationDescription = val;
+            }
+            if (!string.IsNullOrEmpty(eventMetadata["descriptionProperty"]))
+            {
+                if (eventData.GetValueFromPayload<string>("descriptionProperty", (v) => val = v))
+                    meta.Description = val;
+            }
+            if (!string.IsNullOrEmpty(eventMetadata["deploymentNameProperty"]))
+            {
+                if (eventData.GetValueFromPayload<string>("deploymentNameProperty", (v) => val = v))
+                    meta.DeploymentName = val;
+            }
+            if (!string.IsNullOrEmpty(eventMetadata["deploymentVersionProperty"]))
+            {
+                if (eventData.GetValueFromPayload<string>("deploymentVersionProperty", (v) => val = v))
+                    meta.DeploymentVersion = val;
+            }
 
             return DataRetrievalResult.Success;
         }        
